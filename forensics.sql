a6e5441092546d8f0fb7c04f2f6bd772ec3f18d1
K_steve101 <156831030+stee1011@users.noreply.github.com>
2025-10-18 11:48:19 -0300

    Enhance payment variation analysis with employee names
    Added employee names to analysis and included final ranking of suspicious employees.

--- forensics.sql
+++ forensics.sql
@@ -1,16 +1,16 @@
 -- Payment variation from the depatment analysis which will help us know the Departments with the highrst variations for further analysis of corruption or is a talent diversity
 
 with cof_analysis as (
-	select jobtitle, coalesce(stddev(rate),0) as variation_degree_in_payments, coalesce(avg(rate), 0) as mean, (coalesce(stddev(rate),0) / coalesce(avg(rate), 0)) as cof
+	select jobtitle, coalesce(stddev(rate),0) as variation_degree_in_payments, coalesce(avg(rate), 0) as mean, (coalesce(stddev(rate),0) / coalesce(avg(rate), 0)) as cof, employee_name
 	from (
-	select eh.jobtitle, e2.rate
+	select eh.jobtitle, e2.rate, eh.loginid as employee_name
 	from department d 
 	left join employeedepartmenthistory e on d.departmentid = e.departmentid
 	left join employee eh on eh.businessentityid = e.businessentityid 
 	left join employeepayhistory e2 on e2.businessentityid = e.businessentityid
 	) as data_employee
 	
-	group by jobtitle
+	group by jobtitle, employee_name
 	order by variation_degree_in_payments desc
 	
 ),
@@ -30,11 +30,21 @@ cdf_like_data as (
 
 suspicious_dep as (
 select * from cof_analysis cf, cdf_like_data cdf where cf.cof > cdf.Q3 -- which is the Q3 that is 75th percentile
+),
+
+final_data_2 as (
+
+select * from (select * from suspicious_dep se left join employee e on se.jobtitle = e.jobtitle) order by vacationhours desc), -- Further analysis since no way those with more vacation Hours earn high 
+	
+final_data as (
+select  se.jobtitle,se.employee_name , df.cof ,
+row_number() over(partition by se.jobtitle order by df.cof desc) as variation_rank
+from cof_analysis se, suspicious_dep df
+  --- For top 3 Suspicious employees!
+
 )
 
-select * from (select * from suspicious_dep se left join employee e on se.jobtitle = e.jobtitle) order by vacationhours desc  -- Further analysis since no way those with more vacation Hours earn high 
-	
+select * from final_data where variation_rank <= 3   --- For top 3 Suspicious employees!;
 
 
-
-
+-- BYE! Happy Forensics
